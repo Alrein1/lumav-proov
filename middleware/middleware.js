05030479c71f8f4d76d86f716b39e1841cb9264b
Alex-Sander Rein <alexsanderrein@gmail.com>
2025-05-20 06:11:06 +0300

    chore: middleware configuration

---
+++ middleware/middleware.js
@@ -0,0 +1,25 @@
+import cors from "cors";
+import { createServer } from "@vue-storefront/middleware";
+import { integrations } from "./middleware.config.js";
+
+(async () => {
+  const corsMiddleware = cors({
+    origin: [
+      "http://localhost:3000",
+      ...(process.env.MIDDLEWARE_ALLOWED_ORIGINS?.split(",") ?? []),
+    ],
+    credentials: true,
+  });
+
+  const app = await createServer({
+    integrations,
+    middleware: [corsMiddleware], // <-- pass your middleware here
+  });
+
+  const host = process.argv[2] ?? "0.0.0.0";
+  const port = process.argv[3] ?? 8181;
+
+  app.listen(port, host, () => {
+    console.log(`Middleware started: ${host}:${port}`);
+  });
+})();
